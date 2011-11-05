baee35b28be25be8371098028218406317ad80c2
Menghui Wang <wmhkebe@gmail.com>
2011-11-06 00:10:38 +0800

    implement Boat

--- nachos-sjtu/src/nachos/threads/Boat.java
+++ nachos-sjtu/src/nachos/threads/Boat.java
@@ -1,6 +1,8 @@
 package nachos.threads;
 
+import java.util.*;
 import nachos.ag.BoatGrader;
+import nachos.machine.Lib;
 
 public class Boat {
 	static BoatGrader bg;
@@ -18,25 +20,60 @@ public class Boat {
 		// begin(3, 3, b);
 	}
 
+    static private Lock lock;
+    static private Condition cv;
+    
+    static int adultsOahu;
+    static int childrenOahu;
+    static boolean complete;
+    static boolean boatAtOahu, boatAtMolokai;
+    static boolean pilotOahu;
+
 	public static void begin(int adults, int children, BoatGrader b) {
 		// Store the externally generated autograder in a class
 		// variable to be accessible by children.
 		bg = b;
 
 		// Instantiate global variables here
+        lock = new Lock();
+        cv = new Condition(lock);
+        adultsOahu = adults;
+        childrenOahu = children;
+        boatAtOahu = true;
+        boatAtMolokai = false;
+        pilotOahu = false;
+        complete = false;
 
 		// Create threads here. See section 3.4 of the Nachos for Java
 		// Walkthrough linked from the projects page.
+        List<KThread> threads = new ArrayList<KThread>();
 
-		Runnable r = new Runnable() {
-			public void run() {
-				SampleItinerary();
-			}
-		};
-		KThread t = new KThread(r);
-		t.setName("Sample Boat Thread");
-		t.fork();
+        for (int i = 0; i < adults; ++i) {
+            KThread t = new KThread(
+                    new Runnable() {
+                        public void run() {
+                            AdultItinerary();
+                        }
+                    });
+            t.setName("Adult #" + i);
+            threads.add(t);
+            t.fork();
+        }
 
+        for (int i = 0; i < children; ++i) {
+            KThread t = new KThread(
+                    new Runnable() {
+                        public void run() {
+                            ChildItinerary();
+                        }
+                    });
+            t.setName("Child #" + i);
+            threads.add(t);
+            t.fork();
+        }
+
+        for (KThread t: threads)
+            t.join();
 	}
 
 	static void AdultItinerary() {
@@ -46,9 +83,85 @@ public class Boat {
 		 * bg.AdultRowToMolokai(); indicates that an adult has rowed the boat
 		 * across to Molokai
 		 */
+        lock.acquire();
+
+        // Here, the adult is always at Oahu
+        // Because once he got to Molokai
+        // He will exit from this procedure
+        // Therefore he can always access childrenOahu
+        while (!(boatAtOahu && childrenOahu <= 1))
+            cv.sleep();
+
+        --adultsOahu;
+        if (adultsOahu == 0 && childrenOahu == 0)
+            complete = true;
+
+        boatAtOahu = false;
+        bg.AdultRowToMolokai();
+        boatAtMolokai = true;
+        cv.wakeAll();
+
+        Lib.debug('b', "1 adult to Molokai");
+
+        lock.release();
 	}
 
 	static void ChildItinerary() {
+        boolean oahu = true;
+        lock.acquire();
+
+        while (!complete) {
+            if (boatAtOahu && oahu) {
+                if (childrenOahu >= 2) {
+                    if (!pilotOahu) {
+                        pilotOahu = true;
+
+                        oahu = false;
+                    } else {
+                        pilotOahu = false;
+
+                        childrenOahu -= 2;
+                        if (adultsOahu == 0 && childrenOahu == 0)
+                            complete = true;
+
+                        boatAtOahu = false;
+                        bg.ChildRowToMolokai();
+                        bg.ChildRideToMolokai();
+                        boatAtMolokai = true;
+                        oahu = false;
+                        cv.wakeAll();
+
+                        Lib.debug('b', "2 children to Molokai");
+                    }
+                } else if (childrenOahu == 1 && adultsOahu == 0) {
+                    childrenOahu -= 1;
+                    if (adultsOahu == 0 && childrenOahu == 0)
+                        complete = true;
+
+                    boatAtOahu = false;
+                    bg.ChildRowToMolokai();
+                    boatAtMolokai = true;
+                    oahu = false;
+                    cv.wakeAll();
+
+                    Lib.debug('b', "1 child to Molokai");
+                } else
+                    cv.sleep();
+            } else if (boatAtMolokai && !oahu) {
+                childrenOahu += 1;
+
+                boatAtMolokai = false;
+                bg.ChildRowToOahu();
+                boatAtOahu = true;
+                oahu = true;
+                cv.wakeAll();
+
+                Lib.debug('b', "1 child to Oahu");
+            } else
+                cv.sleep();
+        }
+
+        lock.release();
 	}
 
 	static void SampleItinerary() {
@@ -63,5 +176,4 @@ public class Boat {
 		bg.AdultRideToMolokai();
 		bg.ChildRideToMolokai();
 	}
-
 }
